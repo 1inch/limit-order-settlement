8d9970c6911cd9c3b592801dc11a522c24676d54
SevenSwen <tihonenko.v.v@gmail.com>
2022-09-20 16:27:10 +0700

    add ERC20Farmable to SolvingDelegationToken

--- contracts/SolvingDelegationToken.sol
+++ contracts/SolvingDelegationToken.sol
@@ -3,27 +3,29 @@
 pragma solidity 0.8.15;
 
 import "@1inch/delegating/contracts/delegations/DelegateeToken.sol";
+import "@1inch/farming/contracts/ERC20Farmable.sol";
 
-contract SolvingDelegationToken is DelegateeToken {
-    error ApproveDisabled();
-    error TransferDisabled();
-    error TransferFromDisabled();
+contract SolvingDelegationToken is DelegateeToken, ERC20Farmable {
+    error MethodDisabled();
 
-    // solhint-disable-next-line no-empty-blocks
-    constructor(string memory name_, string memory symbol_) DelegateeToken(name_, symbol_) {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 maxUserFarms
+    ) DelegateeToken(name, symbol) ERC20Farmable(maxUserFarms) {} // solhint-disable-line no-empty-blocks
 
     function transfer(
         address, /* to */
         uint256 /* amount */
     ) public pure override(IERC20, ERC20) returns (bool) {
-        revert TransferDisabled();
+        revert MethodDisabled();
     }
 
     function approve(
         address, /* spender */
         uint256 /* amount */
     ) public pure override(IERC20, ERC20) returns (bool) {
-        revert ApproveDisabled();
+        revert MethodDisabled();
     }
 
     function transferFrom(
@@ -31,6 +33,15 @@ contract SolvingDelegationToken is DelegateeToken {
         address, /* to */
         uint256 /* amount */
     ) public pure override(IERC20, ERC20) returns (bool) {
-        revert TransferFromDisabled();
+        revert MethodDisabled();
+    }
+
+    // ERC20 overrides
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20Farmable, ERC20) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
