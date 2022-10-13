7bfb912df5d804a1a1ad0e0dde2409efd58293be
Mikhail Melnik <by.zumzoom@gmail.com>
2022-10-13 11:30:30 +0400

    remove unused interface

--- contracts/interfaces/IRewardableToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.17;
-pragma abicoder v1;
-
-interface IRewardableToken {
-    function balanceOf(address addr) external view returns (uint256);
-}
