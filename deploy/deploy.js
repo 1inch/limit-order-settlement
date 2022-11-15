1d3ada98225ba4efd410df92efa0c9fe343c4d7d
Mikhail Melnik <by.zumzoom@gmail.com>
2022-11-15 16:19:14 +0400

    linter

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -2,8 +2,8 @@ const hre = require('hardhat');
 const { getChainId } = hre;
 
 const INCH = {
-    1: '0x111111111117dC0aa78b770fA6A738034120C302',  // Mainnet
-    137: '0x9c2C5fd7b07E95EE044DDeba0E97a665F142394f'  // Matic
+    1: '0x111111111117dC0aa78b770fA6A738034120C302', // Mainnet
+    137: '0x9c2C5fd7b07E95EE044DDeba0E97a665F142394f', // Matic
 };
 
 const ROUTER_V5_ADDR = '0x1111111254EEB25477B68fb85Ed929f73A960582';
