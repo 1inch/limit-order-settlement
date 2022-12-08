3aad768e61b965c649bde07e149f16c58821e977
SevenSwen <tihonenko.v.v@gmail.com>
2022-12-08 15:55:18 +0700

    add previewPowerOf

--- contracts/helpers/St1inchPreview.sol
+++ contracts/helpers/St1inchPreview.sol
@@ -18,4 +18,18 @@ contract St1inchPreview is VotingPowerCalculator {
         uint256 lockedTill = Math.max(unlockTime, block.timestamp) + duration;
         return _balanceAt(balance + amount, lockedTill) / _VOTING_POWER_DIVIDER;
     }
+
+    function previewPowerOf(address account, uint256 amount, uint256 duration) external view returns (uint256) {
+        return _previewPowerOfAtTime(account, amount, duration, block.timestamp);
+    }
+
+    function previewPowerOfAtTime(address account, uint256 amount, uint256 duration, uint256 timestamp) external view returns (uint256) {
+        return _previewPowerOfAtTime(account, amount, duration, timestamp);
+    }
+
+    function _previewPowerOfAtTime(address account, uint256 amount, uint256 duration, uint256 timestamp) private view returns (uint256) {
+        (uint40 unlockTime, uint216 balance) = st1INCH.depositors(account);
+        uint256 lockedTill = Math.max(unlockTime, block.timestamp) + duration;
+        return _votingPowerAt(_balanceAt(balance + amount, lockedTill) / _VOTING_POWER_DIVIDER, timestamp);
+    }
 }
