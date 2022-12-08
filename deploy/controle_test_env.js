cd7d966a101a30fe50ef8e68a874db00660be8d1
SevenSwen <tihonenko.v.v@gmail.com>
2022-12-08 13:49:00 +0700

    fix linter

--- deploy/controle_test_env.js
+++ deploy/controle_test_env.js
@@ -1,21 +1,6 @@
-const { ether } = require('@1inch/solidity-utils');
-const { getChainId, ethers } = require('hardhat');
+const { getChainId } = require('hardhat');
 const { getContractByAddress } = require('../test/helpers/utils.js');
-const { networks } = require('../hardhat.networks');
 const { setup } = require('../deployments/matic/test_env_setup/setup.js');
-const fs = require('fs');
-
-const deserialize = (path) => {
-    return JSON.parse(
-        fs.readFileSync(path),
-        (key, value) =>
-            ['stake', 'reward'].includes(key)
-                ? BigInt(value)
-                : value,
-    );
-};
-
-const DELEGATORS_PRIVATE_KEYS = process.env.DELEGATORS.split(', ');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = await getChainId();
@@ -23,12 +8,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running controle script');
     console.log('network id ', chainId);
 
-    const provider = new ethers.providers.JsonRpcProvider(networks[deployments.getNetworkName()].url);
-
     const { deployer } = await getNamedAccounts();
 
     const st1inch = await getContractByAddress('St1inch', '0xF93cc6F5ac8E3071519b2c0b90FFb76a49073E3e');
     await (await st1inch.setFeeReceiver(deployer, {
+        maxFeePerGas: setup[chainId].maxFeePerGas,
         maxPriorityFeePerGas: setup[chainId].maxPriorityFeePerGas,
     }));
 };
