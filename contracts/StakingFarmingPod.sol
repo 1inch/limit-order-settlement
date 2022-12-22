75ff0724acbad391c9f67633030ea922ee1f757d
Denis <zzomrot.ne@gmail.com>
2022-12-22 17:58:07 +0400

    Overrided internal method

--- contracts/StakingFarmingPod.sol
+++ contracts/StakingFarmingPod.sol
@@ -3,12 +3,10 @@
 pragma solidity 0.8.17;
 
 import "@1inch/farming/contracts/FarmingPod.sol";
-import "@1inch/farming/contracts/FarmingLib.sol";
 import "./interfaces/ISt1inch.sol";
 
 contract StakingFarmingPod is FarmingPod {
     using SafeERC20 for IERC20;
-    using FarmingLib for FarmingLib.Info;
 
     ISt1inch public immutable st1inch;
 
@@ -16,15 +14,11 @@ contract StakingFarmingPod is FarmingPod {
         st1inch = st1inch_;
     }
 
-    function claim() external override {
-        uint256 podBalance = st1inch.podBalanceOf(address(this), msg.sender);
-        uint256 amount = _farmInfo().claim(msg.sender, podBalance);
-        if (amount > 0) {
-            if (st1inch.emergencyExit()) {
-                st1inch.oneInch().safeTransfer(msg.sender, amount);
-            } else {
-                st1inch.depositFor(msg.sender, amount);
-            }
+    function _transferReward(IERC20 reward, address to, uint256 amount) internal override {
+        if (st1inch.emergencyExit()) {
+            reward.safeTransfer(to, amount);
+        } else {
+            st1inch.depositFor(to, amount);
         }
     }
 }
