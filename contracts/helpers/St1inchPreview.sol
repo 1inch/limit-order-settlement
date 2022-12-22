2a180a38eb5fe497e44d269efdf135bd760f9b8c
Mikhail Melnik <by.zumzoom@gmail.com>
2022-12-22 21:13:08 +0400

    add previewUnlockTime

--- contracts/helpers/St1inchPreview.sol
+++ contracts/helpers/St1inchPreview.sol
@@ -8,6 +8,7 @@ import "../St1inch.sol";
 contract St1inchPreview is VotingPowerCalculator {
     St1inch public immutable st1INCH;
     uint256 private constant _VOTING_POWER_DIVIDER = 20;
+    uint256 private constant _ONE = 1e9;
 
     constructor(St1inch st1INCH_) VotingPowerCalculator(st1INCH_.expBase(), st1INCH_.origin()) {
         st1INCH = st1INCH_;
@@ -27,5 +28,10 @@ contract St1inchPreview is VotingPowerCalculator {
         (, uint40 unlockTime, uint176 balance) = st1INCH.depositors(account);
         uint256 lockedTill = Math.max(unlockTime, block.timestamp) + duration;
         return _votingPowerAt(_balanceAt(balance + amount, lockedTill) / _VOTING_POWER_DIVIDER, timestamp);
+    }
+
+    function previewUnlockTime(address account) public view returns (uint256 allowedExitTime) {
+        (uint40 lockTime, uint40 unlockTime,) = st1INCH.depositors(account);
+        allowedExitTime = lockTime + (unlockTime - lockTime) * st1INCH.minLockPeriodRatio() / _ONE;
     }
 }
