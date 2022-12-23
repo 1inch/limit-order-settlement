87e773f7773473f0549eb16cbb810d0a5d816bb0
Mikhail Melnik <by.zumzoom@gmail.com>
2022-12-23 19:45:51 +0400

    add durationUntilMaxAllowedLoss

--- contracts/helpers/St1inchPreview.sol
+++ contracts/helpers/St1inchPreview.sol
@@ -2,13 +2,16 @@
 
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./VotingPowerCalculator.sol";
 import "../St1inch.sol";
 
-contract St1inchPreview is VotingPowerCalculator {
+contract St1inchPreview is VotingPowerCalculator, Ownable {
     St1inch public immutable st1INCH;
     uint256 private constant _VOTING_POWER_DIVIDER = 20;
     uint256 private constant _ONE = 1e9;
+    uint256 public durationUntilMaxAllowedLoss;  // log(0.95 * maxAllowedLoss + 0.05) / log(baseExp)
 
     constructor(St1inch st1INCH_) VotingPowerCalculator(st1INCH_.expBase(), st1INCH_.origin()) {
         st1INCH = st1INCH_;
@@ -33,5 +36,10 @@ contract St1inchPreview is VotingPowerCalculator {
     function previewUnlockTime(address account) public view returns (uint256 allowedExitTime) {
         (uint40 lockTime, uint40 unlockTime,) = st1INCH.depositors(account);
         allowedExitTime = lockTime + (unlockTime - lockTime) * st1INCH.minLockPeriodRatio() / _ONE;
+        allowedExitTime = Math.max(allowedExitTime, unlockTime - 730 days + durationUntilMaxAllowedLoss);
+    }
+
+    function setDurationUntilMaxAllowedLoss(uint256 duration) external onlyOwner {
+        durationUntilMaxAllowedLoss = duration;
     }
 }
