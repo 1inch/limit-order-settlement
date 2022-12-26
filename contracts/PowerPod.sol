d511d957132e87ca42eda6358f21461af1e5744f
Mikhail Melnik <by.zumzoom@gmail.com>
2022-12-26 17:20:21 +0400

    fix

--- contracts/PowerPod.sol
+++ contracts/PowerPod.sol
@@ -9,7 +9,7 @@ import "./St1inch.sol";
 
 contract PowerPod is FarmingDelegationPod, VotingPowerCalculator, IVotable {
     uint256 private constant _MAX_SHARE_PODS = 3;
-    uint256 private constant _SHARE_POD_GAS_LIMIT = 150_000;
+    uint256 private constant _SHARE_POD_GAS_LIMIT = 140_000;
 
     constructor(string memory name_, string memory symbol_, St1inch st1inch)
         FarmingDelegationPod(name_, symbol_, st1inch, _MAX_SHARE_PODS, _SHARE_POD_GAS_LIMIT)
