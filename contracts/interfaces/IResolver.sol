b3f6f9e3bffdb47a18f87ad379fe7ae1e94f6288
Mikhail Melnik <by.zumzoom@gmail.com>
2023-02-20 19:36:38 +0400

    remove unused import

--- contracts/interfaces/IResolver.sol
+++ contracts/interfaces/IResolver.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.17;
 
-import "../libraries/DynamicSuffix.sol";
-
 interface IResolver {
     function resolveOrders(address resolver, bytes calldata tokensAndAmounts, bytes calldata data) external;
 }
