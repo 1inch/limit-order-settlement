336643e21178a8da31ab6082a2e65a7554ed5d21
Mikhail Melnik <by.zumzoom@gmail.com>
2023-03-17 14:47:36 +0400

    add custom fields test

---
+++ test/FusionDetailsMock.js
@@ -0,0 +1,57 @@
+const { ethers } = require('hardhat');
+const { BigNumber } = require('ethers');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect, constants, time } = require('@1inch/solidity-utils');
+const { buildFusion } = require('./helpers/fusionUtils');
+
+describe('FusionDetailsMock', function () {
+    async function initContracts() {
+        const FusionDetailsMock = await ethers.getContractFactory('FusionDetailsMock');
+        const fusionDetailsMock = await FusionDetailsMock.deploy();
+        await fusionDetailsMock.deployed();
+        return { fusionDetailsMock };
+    }
+
+    it('should return default values', async function () {
+        const { fusionDetailsMock } = await loadFixture(initContracts);
+
+        const details = buildFusion();
+        const result = await fusionDetailsMock.parse(details, constants.ZERO_ADDRESS);
+
+        expect(Object.assign({}, result)).to.deep.contain({
+            detailsLength: BigNumber.from('19'),
+            takingFeeReceiver: constants.ZERO_ADDRESS,
+            takingFeeAmount: BigNumber.from('0'),
+            bump: BigNumber.from('0'),
+            resolverFee: BigNumber.from('0'),
+            isValidResolver: false,
+        });
+    });
+
+    it('should return custom values', async function () {
+        const { fusionDetailsMock } = await loadFixture(initContracts);
+
+        const details = buildFusion({
+            resolvers: [fusionDetailsMock.address],
+            points: [[10n, 100n], [5n, 50n]],
+            timeStart: (await time.latest()) + 100,
+            duration: time.duration.hours(2),
+            initialRateBump: 200n,
+            resolverFee: 10000n,
+            publicTimeLimit: (await time.latest()) + time.duration.hours(1),
+        });
+
+        await time.increase(time.duration.minutes(2));
+
+        const result = await fusionDetailsMock.parse(details, fusionDetailsMock.address);
+
+        expect(Object.assign({}, result)).to.deep.include({
+            detailsLength: BigNumber.from('43'),
+            takingFeeReceiver: constants.ZERO_ADDRESS,
+            takingFeeAmount: BigNumber.from('0'),
+            bump: BigNumber.from('49'),
+            resolverFee: BigNumber.from('10000'),
+            isValidResolver: true
+        });
+    });
+});
