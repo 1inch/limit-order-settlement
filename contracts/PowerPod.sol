c4a900606424f90a6d49c9bded0b1b704e580b12
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-06-02 18:57:12 -0300

    upd powerpod natspec

--- contracts/PowerPod.sol
+++ contracts/PowerPod.sol
@@ -7,6 +7,11 @@ import "@1inch/st1inch/contracts/helpers/VotingPowerCalculator.sol";
 import "@1inch/st1inch/contracts/interfaces/IVotable.sol";
 import "@1inch/st1inch/contracts/interfaces/ISt1inch.sol";
 
+/**
+ * @title PowerPod
+ * @notice The contract combines farming and delegation features of pods with voting power calculations for the participating accounts.
+ * @dev Limits pods number and the gas usage per pod.
+ */
 contract PowerPod is FarmingDelegationPod, VotingPowerCalculator, IVotable {
     uint256 private constant _MAX_SHARE_PODS = 3;
     uint256 private constant _SHARE_POD_GAS_LIMIT = 140_000;
@@ -16,7 +21,12 @@ contract PowerPod is FarmingDelegationPod, VotingPowerCalculator, IVotable {
         VotingPowerCalculator(st1inch.expBase(), st1inch.origin())
     {}
 
-    function votingPowerOf(address account) external view virtual returns (uint256) {
+    /**
+     * @dev Returns the voting power of the specified account at the current block.
+     * @param account The account to get the voting power for.
+     * @return /votingPower/ The voting power of the account.
+     */
+    function votingPowerOf(address account) external view virtual returns (uint256 /* votingPower */) {
         return _votingPowerAt(balanceOf(account), block.timestamp);
     }
 }
