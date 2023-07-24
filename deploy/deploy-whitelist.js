262a2a2642474d3722e0ffe14f499c9b6125cdba
xenia <xenia.shape@gmail.com>
2023-07-24 18:23:08 +0100

    Fixed lint

--- deploy/deploy-whitelist.js
+++ deploy/deploy-whitelist.js
@@ -2,7 +2,7 @@ const hre = require('hardhat');
 const { getChainId } = hre;
 const { idempotentDeployGetContract } = require('../test/helpers/utils.js');
 
-const DAO_ADDRESS = '0x7951c7ef839e26F63DA87a42C9a87986507f1c07'
+const DAO_ADDRESS = '0x7951c7ef839e26F63DA87a42C9a87986507f1c07';
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = await getChainId();
@@ -27,7 +27,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const tx = await whitelist.transferOwnership(DAO_ADDRESS);
     await tx.wait();
-    console.log(`Ownership has been successfully transferred to ${DAO_ADDRESS}`)
+    console.log(`Ownership has been successfully transferred to ${DAO_ADDRESS}`);
 };
 
 module.exports.skip = async () => true;
