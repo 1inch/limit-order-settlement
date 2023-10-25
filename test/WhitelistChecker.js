60064841507930762f4e731ef5070a5dc90a0a0e
Denis <zzomrot.ne@gmail.com>
2023-10-26 00:16:59 +0400

    Fix WhitelistChecker tests

--- test/WhitelistChecker.js
+++ test/WhitelistChecker.js
@@ -1,15 +1,21 @@
 const { ethers } = require('hardhat');
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { signOrder, buildOrder, compactSignature, fillWithMakingAmount } = require('@1inch/limit-order-protocol-contract/test/helpers/orderUtils');
-const { expect, ether, trim0x, deployContract } = require('@1inch/solidity-utils');
+const { loadFixture, time } = require('@nomicfoundation/hardhat-network-helpers');
+const { buildOrder, buildMakerTraits } = require('@1inch/limit-order-protocol-contract/test/helpers/orderUtils');
+const { expect, ether, deployContract, constants } = require('@1inch/solidity-utils');
 const { deploySwapTokens, getChainId } = require('./helpers/fixtures');
-const { buildFusions } = require('./helpers/fusionUtils');
+const { buildCalldataForOrder } = require('./helpers/fusionUtils');
 
-describe.skip('WhitelistChecker // TODO: Update this tests', function () {
+describe('WhitelistChecker // TODO: Update this tests', function () {
     async function initContracts() {
         const [owner, alice] = await ethers.getSigners();
         const chainId = await getChainId();
         const { dai, weth, lopv4 } = await deploySwapTokens();
+
+        const whitelistRegistrySimple = await deployContract('WhitelistRegistrySimple', []);
+        const settlement = await deployContract('SettlementExtension', [lopv4.address, weth.address]);
+
+        const ResolverMock = await ethers.getContractFactory('ResolverMock');
+        const resolver = await ResolverMock.deploy(settlement.address, lopv4.address);
 
         await dai.mint(owner.address, ether('100'));
         await dai.mint(alice.address, ether('100'));
@@ -21,141 +27,144 @@ describe.skip('WhitelistChecker // TODO: Update this tests', function () {
         await weth.approve(lopv4.address, ether('1'));
         await weth.connect(alice).approve(lopv4.address, ether('1'));
 
-        const whitelistRegistrySimple = await deployContract('WhitelistRegistrySimple', []);
-        const settlement = await deployContract('SettlementExtension', [lopv4.address, weth.address]);
+        await resolver.approve(dai.address, lopv4.address);
+        await resolver.approve(weth.address, lopv4.address);
 
-        const ResolverMock = await ethers.getContractFactory('ResolverMock');
-        const resolver = await ResolverMock.deploy(settlement.address, lopv4.address);
+        const auctionStartTime = await time.latest();
+        const auctionDetails = ethers.utils.solidityPack(
+            ['uint32', 'uint24', 'uint24'], [auctionStartTime, time.duration.hours(1), 0],
+        );
 
         return {
             contracts: { dai, weth, lopv4, whitelistRegistrySimple, settlement, resolver },
             accounts: { owner, alice },
-            others: { chainId },
+            others: { chainId, auctionDetails, auctionStartTime },
         };
     }
 
     describe('should not work with non-whitelisted address', function () {
-        it('whitelist check in settleOrders method', async function () {
-            const { contracts: { dai, weth, lopv4, settlement }, accounts: { owner, alice }, others: { chainId } } = await loadFixture(initContracts);
+        it('whitelist check in postInteraction method', async function () {
+            const dataFormFixture = await loadFixture(initContracts);
+            const {
+                contracts: { dai, weth, resolver },
+                accounts: { alice },
+            } = dataFormFixture;
 
-            const { fusions: [fusionDetails], hashes: [fusionHash], resolvers } = await buildFusions([{}]);
+            weth.transfer(resolver.address, ether('0.1'));
 
-            const order = buildOrder({
-                makerAsset: dai.address,
-                takerAsset: weth.address,
-                makingAmount: ether('10'),
-                takingAmount: ether('0.01'),
-                maker: alice.address,
+            const fillOrderToData = await buildCalldataForOrder({
+                orderData: {
+                    maker: alice.address,
+                    makerAsset: dai.address,
+                    takerAsset: weth.address,
+                    makingAmount: ether('100'),
+                    takingAmount: ether('0.1'),
+                    makerTraits: buildMakerTraits(),
+                },
+                orderSigner: alice,
+                dataFormFixture,
+                minReturn: ether('0.1'),
+                isInnermostOrder: true,
+                whitelistData: '0x' + constants.ZERO_ADDRESS.substring(22),
             });
-            order.salt = fusionHash;
 
-            const { r, vs } = compactSignature(await signOrder(order, chainId, lopv4.address, alice));
-            const fillOrderToData = lopv4.interface.encodeFunctionData('fillOrderTo', [
-                order,
-                r,
-                vs,
-                ether('10'),
-                fillWithMakingAmount('0'),
-                owner.address,
-                settlement.address + '01' + trim0x(fusionDetails),
-            ]) + trim0x(resolvers);
-
-            await expect(settlement.settleOrders(fillOrderToData))
-                .to.be.revertedWithCustomError(settlement, 'ResolverIsNotWhitelisted');
+            try {
+                await resolver.settleOrders(fillOrderToData);
+                expect.fail('should revert');
+            } catch (e) {
+                expect(e.message).to.include('FailedExternalCall');
+                expect(e.message).to.include('0x4b576069'); // ResolverIsNotWhitelisted()
+            }
         });
 
-        it('onlyThis modifier in takerInteraction method', async function () {
-            const { contracts: { dai, weth, lopv4, settlement }, accounts: { owner, alice }, others: { chainId } } = await loadFixture(initContracts);
+        it('only resolver can use takerInteraction method', async function () {
+            const dataFormFixture = await loadFixture(initContracts);
+            const {
+                contracts: { dai, weth, resolver, settlement, lopv4 },
+                accounts: { alice },
+            } = dataFormFixture;
+
+            // Deploy another resolver
+            const ResolverMock = await ethers.getContractFactory('ResolverMock');
+            const fakeResolver = await ResolverMock.deploy(settlement.address, lopv4.address);
+
+            const fillOrderToData = await buildCalldataForOrder({
+                orderData: {
+                    maker: alice.address,
+                    makerAsset: dai.address,
+                    takerAsset: weth.address,
+                    makingAmount: ether('100'),
+                    takingAmount: ether('0.1'),
+                    makerTraits: buildMakerTraits(),
+                },
+                orderSigner: alice,
+                dataFormFixture,
+                minReturn: ether('0.1'),
+                isInnermostOrder: true,
+            });
+
+            // Change resolver to fakeResolver in takerInteraction
+            const fakeFillOrderToData = fillOrderToData.slice(0, fillOrderToData.length - 86) + fakeResolver.address.substring(2) + fillOrderToData.slice(-46);
+
+            try {
+                await resolver.settleOrders(fakeFillOrderToData);
+                expect.fail('should revert');
+            } catch (e) {
+                expect(e.message).to.include('FailedExternalCall');
+                expect(e.message).to.include('0x5211a079'); // NotTaker()
+            }
+        });
+
+        it('only LOP can use takerInteraction method', async function () {
+            const dataFormFixture = await loadFixture(initContracts);
+            const {
+                contracts: { dai, weth, resolver, lopv4 },
+                accounts: { alice },
+            } = dataFormFixture;
 
             const order = buildOrder({
+                maker: alice.address,
                 makerAsset: dai.address,
                 takerAsset: weth.address,
                 makingAmount: ether('100'),
                 takingAmount: ether('0.1'),
-                maker: alice.address,
-            });
-
-            const { r, vs } = compactSignature(await signOrder(order, chainId, lopv4.address, alice));
-            await expect(lopv4.fillOrderTo(order, r, vs, ether('10'), fillWithMakingAmount('0'), owner.address, settlement.address + '01'))
-                .to.be.revertedWithCustomError(settlement, 'AccessDenied');
-        });
-
-        it('onlyLimitOrderProtocol modifier', async function () {
-            const { contracts: { dai, weth, lopv4, settlement }, accounts: { owner, alice } } = await loadFixture(initContracts);
-
-            const order = buildOrder({
-                makerAsset: dai.address,
-                takerAsset: weth.address,
-                makingAmount: ether('100'),
-                takingAmount: ether('0.1'),
-                maker: alice.address,
             });
             const orderHash = await lopv4.hashOrder(order);
 
-            await expect(settlement.takerInteraction(order, orderHash, owner.address, '1', '1', '0', '0x'))
-                .to.be.revertedWithCustomError(settlement, 'AccessDenied');
+            await expect(resolver.takerInteraction(order, '0x', orderHash, alice.address, '0', '0', '0', '0x'))
+                .to.be.revertedWithCustomError(resolver, 'OnlyLOP');
         });
     });
 
     describe('should work with whitelisted address', function () {
-        async function initContractsAndSetStatus() {
-            const data = await initContracts();
-            const { contracts: { whitelistRegistrySimple }, accounts: { owner } } = data;
-            await whitelistRegistrySimple.setStatus(owner.address, true);
-            return data;
-        }
-
         it('whitelist check in settleOrders method', async function () {
-            const { contracts: { dai, weth, lopv4, settlement, resolver }, accounts: { owner, alice }, others: { chainId } } = await loadFixture(initContractsAndSetStatus);
+            const dataFormFixture = await loadFixture(initContracts);
+            const {
+                contracts: { dai, weth, resolver },
+                accounts: { alice },
+            } = dataFormFixture;
 
-            const { fusions: [fusionDetails0, fusionDetails1], hashes: [fusionHash0, fusionHash1], resolvers } = await buildFusions([
-                { resolvers: [resolver.address] },
-                { resolvers: [resolver.address] },
-            ]);
+            weth.transfer(resolver.address, ether('0.1'));
 
-            const order0 = buildOrder({
-                makerAsset: dai.address,
-                takerAsset: weth.address,
-                makingAmount: ether('100'),
-                takingAmount: ether('0.1'),
-                maker: owner.address,
+            const fillOrderToData = await buildCalldataForOrder({
+                orderData: {
+                    maker: alice.address,
+                    makerAsset: dai.address,
+                    takerAsset: weth.address,
+                    makingAmount: ether('100'),
+                    takingAmount: ether('0.1'),
+                    makerTraits: buildMakerTraits(),
+                },
+                orderSigner: alice,
+                dataFormFixture,
+                minReturn: ether('0.1'),
+                isInnermostOrder: true,
+                whitelistData: '0x' + resolver.address.substring(22),
             });
-            order0.salt = fusionHash0;
 
-            const order1 = buildOrder({
-                makerAsset: weth.address,
-                takerAsset: dai.address,
-                makingAmount: ether('0.1'),
-                takingAmount: ether('100'),
-                maker: alice.address,
-            });
-            order1.salt = fusionHash1;
-
-            const { r: r1, vs: vs1 } = compactSignature(await signOrder(order1, chainId, lopv4.address, alice));
-            const fillOrderToData1 = lopv4.interface.encodeFunctionData('fillOrderTo', [
-                order1,
-                r1,
-                vs1,
-                ether('0.1'),
-                fillWithMakingAmount('0'),
-                resolver.address,
-                settlement.address + '01' + trim0x(fusionDetails1),
-            ]);
-
-            const { r: r0, vs: vs0 } = compactSignature(await signOrder(order0, chainId, lopv4.address, owner));
-            const fillOrderToData0 = lopv4.interface.encodeFunctionData('fillOrderTo', [
-                order0,
-                r0,
-                vs0,
-                ether('100'),
-                fillWithMakingAmount('0'),
-                resolver.address,
-                settlement.address + '00' + trim0x(fusionDetails0) + trim0x(fillOrderToData1),
-            ]) + trim0x(resolvers);
-
-            const txn = await resolver.settleOrders(fillOrderToData0);
-            await expect(txn).to.changeTokenBalances(dai, [owner, alice], [ether('-100'), ether('100')]);
-            await expect(txn).to.changeTokenBalances(weth, [owner, alice], [ether('0.1'), ether('-0.1')]);
+            const txn = await resolver.settleOrders(fillOrderToData);
+            await expect(txn).to.changeTokenBalances(dai, [alice, resolver], [ether('-100'), ether('100')]);
+            await expect(txn).to.changeTokenBalances(weth, [alice, resolver], [ether('0.1'), ether('-0.1')]);
         });
     });
 });
--- test/helpers/fusionUtils.js
+++ test/helpers/fusionUtils.js
@@ -1,146 +1,68 @@
-const { time, trim0x } = require('@1inch/solidity-utils');
-const { assert } = require('chai');
-const { keccak256 } = require('ethers/lib/utils');
+const { trim0x } = require('@1inch/solidity-utils');
+const { ethers } = require('hardhat');
+const { buildOrder, signOrder, buildTakerTraits } = require('@1inch/limit-order-protocol-contract/test/helpers/orderUtils');
 
-async function buildFusions(params) {
-    const allResolvers = [...new Set(params.flatMap((param) => param.resolvers).filter(item => item !== undefined))];
-    const currentTime = await time.latest();
-    const fusions = [];
+async function buildCalldataForOrder({
+    orderData,
+    orderSigner,
+    minReturn,
+    dataFormFixture,
+    additionalDataForSettlement = '',
+    isInnermostOrder = false,
+    isMakingAmount = true,
+    fillingAmount = isMakingAmount ? orderData.makingAmount : orderData.takingAmount,
+    feeType = 0,
+    integrator = orderSigner.address,
+    resolverFee = 0,
+    auctionDetails = dataFormFixture.others.auctionDetails,
+    whitelistData = '0x' + dataFormFixture.contracts.resolver.address.substring(22),
+}) {
+    const {
+        contracts: { lopv4, settlement, resolver },
+        others: { chainId, auctionStartTime },
+    } = dataFormFixture;
 
-    for (let {
-        resolvers = [],
-        points = [],
-        startTime,
-        auctionDelay = 0n,
-        auctionDuration = time.duration.hours(1),
-        initialRateBump = 0n,
-        resolverFee = 0n,
-        publicTimeDelay,
-        takerFee = 0n,
-        takerFeeReceiver = undefined,
-    } of params) {
-        if (!startTime) {
-            startTime = currentTime;
-        }
-
-        if (!publicTimeDelay) {
-            publicTimeDelay = auctionDuration >> 1;
-        }
-
-        // Order `interaction` prefix structure:
-        // struct Data {
-        //     bytes1 flags;
-        //     bytes4 startTime;
-        //     bytes2 auctionDelay;
-        //     bytes3 auctionDuration;
-        //     bytes3 initialRateBump;
-        //     bytes4 resolverFee;
-        //     bytes2 publicTimeDelay;
-        //     (bytes2,bytes10)[N] resolversAndTimeDeltas;
-        //     (bytes2,bytes3)[M] pointsAndTimeDeltas;
-        //     bytes24? takingFee; // optional if flags has _HAS_TAKING_FEE_FLAG
-        // }
-        assert(resolvers.length <= 15, 'Too many resolvers');
-        assert(points.length <= 7, 'Too many points');
-        assert(BigInt(startTime) < (1n << 32n), 'Auction start is too big');
-        assert(BigInt(auctionDelay) < (1n << 16n), 'Auction delay is too big');
-        assert(BigInt(auctionDuration) < (1n << 24n), 'Auction duration is too big');
-        assert(BigInt(initialRateBump) < (1n << 24n), 'Initial rate bump is too big');
-        assert(BigInt(resolverFee) < (1n << 32n), 'Resolver fee is too big');
-        assert(BigInt(publicTimeDelay) < (1n << 16n), 'Public time delay is too big');
-
-        const flags = (takerFee > 0 ? 0x80 : 0) | (resolvers.length << 3) | points.length;
-
-        fusions.push(
-            '0x' + flags.toString(16).padStart(2, '0') +
-            startTime.toString(16).padStart(8, '0') +
-            auctionDelay.toString(16).padStart(4, '0') +
-            auctionDuration.toString(16).padStart(6, '0') +
-            initialRateBump.toString(16).padStart(6, '0') +
-            resolverFee.toString(16).padStart(8, '0') +
-            publicTimeDelay.toString(16).padStart(4, '0') +
-            resolvers.map((resolver, i) => {
-                const delta = i === 0 ? 0 : auctionDuration / resolvers.length;
-                assert(BigInt(delta) < (1n << 16n), 'Resolver time delta is too big');
-                return allResolvers.indexOf(resolver).toString(16).padStart(2, '0') + delta.toString(16).padStart(4, '0');
-            }).join('') +
-            points.map(([delta, bump]) => {
-                assert(BigInt(delta) < (1n << 16n), 'Point time delta is too big');
-                return bump.toString(16).padStart(6, '0') + delta.toString(16).padStart(4, '0');
-            }).join('') +
-            (takerFee > 0 ? takerFee.toString(16).padStart(8, '0') + trim0x(takerFeeReceiver) : ''),
-        );
+    let postInteractionFeeDataTypes = ['uint8'];
+    let postInteractionFeeData = [0];
+    if (feeType === 1) {
+        postInteractionFeeDataTypes = [...postInteractionFeeDataTypes, 'bytes4'];
+        postInteractionFeeData = [feeType, '0x' + resolverFee.toString(16).padStart(8, '0')];
+    }
+    if (feeType === 2) {
+        postInteractionFeeDataTypes = [...postInteractionFeeDataTypes, 'bytes20', 'bytes4'];
+        postInteractionFeeData = [feeType, integrator, '0x' + resolverFee.toString(16).padStart(8, '0')];
     }
 
-    return {
-        fusions,
-        hashes: (await Promise.all(params.map(buildFusion))).map(keccak256),
-        resolvers: '0x' + allResolvers.map((resolver) => resolver.substring(22)).join('') + allResolvers.length.toString(16).padStart(2, '0'),
-    };
-}
+    const order = buildOrder(orderData, {
+        makingAmountData: settlement.address + trim0x(auctionDetails),
+        takingAmountData: settlement.address + trim0x(auctionDetails),
+        postInteraction: settlement.address +
+            trim0x(ethers.utils.solidityPack(postInteractionFeeDataTypes, postInteractionFeeData)) +
+            trim0x(ethers.utils.solidityPack(['uint32', 'bytes10', 'uint16'], [auctionStartTime, whitelistData, 0])),
+    });
 
-async function buildFusion({
-    resolvers = [],
-    points = [],
-    startTime,
-    auctionDelay = 0n,
-    auctionDuration = time.duration.hours(1),
-    initialRateBump = 0n,
-    resolverFee = 0n,
-    publicTimeDelay,
-    takerFee = 0n,
-    takerFeeReceiver = undefined,
-} = {}) {
-    if (!startTime) {
-        startTime = await time.latest();
-    }
+    const { r, _vs: vs } = ethers.utils.splitSignature(await signOrder(order, chainId, lopv4.address, orderSigner));
 
-    if (!publicTimeDelay) {
-        publicTimeDelay = auctionDuration >> 1;
-    }
+    await resolver.approve(order.takerAsset, lopv4.address);
 
-    // Order `interaction` prefix structure:
-    // struct Data {
-    //     bytes1 flags;
-    //     bytes4 startTime;
-    //     bytes2 auctionDelay;
-    //     bytes3 auctionDuration;
-    //     bytes3 initialRateBump;
-    //     bytes4 resolverFee;
-    //     bytes2 publicTimeDelay;
-    //     (bytes2,bytes10)[N] resolversAndTimeDeltas;
-    //     (bytes2,bytes3)[M] pointsAndTimeDeltas;
-    //     bytes24? takingFee; // optional if flags has _HAS_TAKING_FEE_FLAG
-    // }
-    assert(resolvers.length <= 15, 'Too many resolvers');
-    assert(points.length <= 7, 'Too many points');
-    assert(BigInt(startTime) < (1n << 32n), 'Auction start is too big');
-    assert(BigInt(auctionDelay) < (1n << 16n), 'Auction delay is too big');
-    assert(BigInt(auctionDuration) < (1n << 24n), 'Auction duration is too big');
-    assert(BigInt(initialRateBump) < (1n << 24n), 'Initial rate bump is too big');
-    assert(BigInt(resolverFee) < (1n << 32n), 'Resolver fee is too big');
-    assert(BigInt(publicTimeDelay) < (1n << 16n), 'Public time delay is too big');
+    const takerTraits = buildTakerTraits({
+        makingAmount: isMakingAmount,
+        minReturn,
+        extension: order.extension,
+        interaction: resolver.address + (isInnermostOrder ? '01' : '00') + trim0x(additionalDataForSettlement),
+        target: resolver.address,
+    });
 
-    const flags = (takerFee > 0 ? 0x80 : 0) | (resolvers.length << 3) | points.length;
-    return '0x' + flags.toString(16).padStart(2, '0') +
-        startTime.toString(16).padStart(8, '0') +
-        auctionDelay.toString(16).padStart(4, '0') +
-        auctionDuration.toString(16).padStart(6, '0') +
-        initialRateBump.toString(16).padStart(6, '0') +
-        resolverFee.toString(16).padStart(8, '0') +
-        publicTimeDelay.toString(16).padStart(4, '0') +
-        resolvers.map((resolver, i) => {
-            const delta = i === 0 ? 0 : auctionDuration / resolvers.length;
-            assert(BigInt(delta) < (1n << 16n), 'Resolver time delta is too big');
-            return resolver.substring(22) + delta.toString(16).padStart(4, '0');
-        }).join('') +
-        points.map(([delta, bump]) => {
-            assert(BigInt(delta) < (1n << 16n), 'Point time delta is too big');
-            return bump.toString(16).padStart(6, '0') + delta.toString(16).padStart(4, '0');
-        }).join('') +
-        (takerFee > 0 ? takerFee.toString(16).padStart(8, '0') + trim0x(takerFeeReceiver) : '');
+    return lopv4.interface.encodeFunctionData('fillOrderArgs', [
+        order,
+        r,
+        vs,
+        fillingAmount,
+        takerTraits.traits,
+        takerTraits.args,
+    ]);
 }
 
 module.exports = {
-    buildFusions,
+    buildCalldataForOrder,
 };
