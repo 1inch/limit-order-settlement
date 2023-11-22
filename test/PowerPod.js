b3a58ff49e1fb5edd4957662f59723f4abc734a6
Denis <zzomrot.ne@gmail.com>
2023-11-22 20:56:31 +0400

    Patch PowerPod tests

--- test/PowerPod.js
+++ test/PowerPod.js
@@ -10,20 +10,19 @@ const BALANCE_THRESHOLD = 1000; // 10%
 describe('PowerPod', function () {
     const stakeAndRegisterInDelegation = async (st1inch, delegation, user, amount, userIndex) => {
         await st1inch.connect(user).deposit(0, COMMON_LOCK_DURATION);
-        await st1inch.depositFor(user.address, amount);
-        await st1inch.connect(user).addPod(delegation.address);
+        await st1inch.depositFor(user, amount);
+        await st1inch.connect(user).addPod(delegation);
         await delegation
-            .connect(user)
-            .functions['register(string,string)'](
+            .connect(user)['register(string,string)'](
                 `${userIndex}DelegatingToken`,
                 `A${userIndex}DT`,
             );
-        await delegation.connect(user).delegate(user.address);
+        await delegation.connect(user).delegate(user);
     };
 
     const depositAndDelegateTo = async (st1inch, delegation, from, to, amount, duration = COMMON_LOCK_DURATION) => {
         await st1inch.connect(from).deposit(amount, duration);
-        await st1inch.connect(from).addPod(delegation.address);
+        await st1inch.connect(from).addPod(delegation);
         await delegation.connect(from).delegate(to);
     };
 
@@ -31,16 +30,16 @@ describe('PowerPod', function () {
         const accounts = await ethers.getSigners();
         const [owner, alice, whitelistedUser1, /* whitelistedUser2 */, /* whitelistedUser3 */, clearAddress] = accounts;
 
-        const oneInch = await deployContract('ERC20PermitMock', ['1inch', '1inch', owner.address, ether('200')]);
-        await oneInch.transfer(alice.address, ether('100'));
+        const oneInch = await deployContract('ERC20PermitMock', ['1inch', '1inch', owner, ether('200')]);
+        await oneInch.transfer(alice, ether('100'));
 
-        const st1inch = await deployContract('St1inch', [oneInch.address, expBase, owner.address]);
-        await oneInch.approve(st1inch.address, ether('100'));
-        await oneInch.connect(alice).approve(st1inch.address, ether('100'));
+        const st1inch = await deployContract('St1inch', [oneInch, expBase, owner]);
+        await oneInch.approve(st1inch, ether('100'));
+        await oneInch.connect(alice).approve(st1inch, ether('100'));
 
-        const delegation = await deployContract('PowerPod', ['PowerPod', 'PP', st1inch.address]);
+        const delegation = await deployContract('PowerPod', ['PowerPod', 'PP', st1inch]);
 
-        const whitelistRegistry = await deployContract('WhitelistRegistry', [delegation.address, BALANCE_THRESHOLD]);
+        const whitelistRegistry = await deployContract('WhitelistRegistry', [delegation, BALANCE_THRESHOLD]);
         // fill all whitelist into WhitelistRegistry
         for (let i = 0; i < MAX_WHITELISTED; ++i) {
             const userIndex = i + 2;
@@ -59,12 +58,12 @@ describe('PowerPod', function () {
     describe('Should calculate voting power', function () {
         it('for account with 0 balance', async function () {
             const { contracts: { delegation }, accounts: { clearAddress } } = await loadFixture(initContracts);
-            expect(await delegation.votingPowerOf(clearAddress.address)).to.equal(0);
+            expect(await delegation.votingPowerOf(clearAddress)).to.equal(0);
         });
 
         it('for account with st1inch balance', async function () {
             const { contracts: { st1inch, delegation }, accounts: { alice } } = await loadFixture(initContracts);
-            expect(await delegation.votingPowerOf(alice.address)).to.equal(await st1inch.votingPowerOf(alice.address));
+            expect(await delegation.votingPowerOf(alice)).to.equal(await st1inch.votingPowerOf(alice));
         });
     });
 
@@ -80,7 +79,7 @@ describe('PowerPod', function () {
                 'BalanceLessThanThreshold',
             );
             // create another stake and delegate to owner
-            await depositAndDelegateTo(st1inch, delegation, alice, owner.address, ether('2'));
+            await depositAndDelegateTo(st1inch, delegation, alice, owner, ether('2'));
             // register owner into whitelistRegistry and chack that
             await whitelistRegistry.register();
             expect(await whitelistRegistry.getWhitelist()).to.contain(owner.address);
@@ -92,8 +91,8 @@ describe('PowerPod', function () {
                 accounts: { owner, alice },
             } = await loadFixture(initContracts);
             // delegate to owner and deposit 1inch
-            await st1inch.connect(alice).addPod(delegation.address);
-            await delegation.connect(alice).delegate(owner.address);
+            await st1inch.connect(alice).addPod(delegation);
+            await delegation.connect(alice).delegate(owner);
             await st1inch.connect(alice).deposit(ether('2'), COMMON_LOCK_DURATION);
 
             await whitelistRegistry.register();
@@ -105,16 +104,16 @@ describe('PowerPod', function () {
                 accounts: { owner, alice },
             } = await loadFixture(initContracts);
             const DelegatedShare = await ethers.getContractFactory('DelegatedShare');
-            const delegatedShare = DelegatedShare.attach(await delegation.registration(owner.address));
-            const balanceDelegated = await delegatedShare.balanceOf(owner.address);
+            const delegatedShare = DelegatedShare.attach(await delegation.registration(owner));
+            const balanceDelegated = await delegatedShare.balanceOf(owner);
             expect(await delegatedShare.totalSupply()).to.equal(balanceDelegated);
-            expect(await st1inch.balanceOf(owner.address)).to.equal(balanceDelegated);
+            expect(await st1inch.balanceOf(owner)).to.equal(balanceDelegated);
 
-            await depositAndDelegateTo(st1inch, delegation, alice, owner.address, ether('2'));
+            await depositAndDelegateTo(st1inch, delegation, alice, owner, ether('2'));
 
-            const balanceDelegator = await delegatedShare.balanceOf(alice.address);
-            expect(balanceDelegated.mul(await st1inch.balanceOf(alice.address))).to.equal(
-                balanceDelegator.mul(await st1inch.balanceOf(owner.address)),
+            const balanceDelegator = await delegatedShare.balanceOf(alice);
+            expect(balanceDelegated * (await st1inch.balanceOf(alice))).to.equal(
+                balanceDelegator * (await st1inch.balanceOf(owner)),
             );
         });
 
@@ -123,7 +122,7 @@ describe('PowerPod', function () {
                 contracts: { st1inch, delegation, whitelistRegistry },
                 accounts: { owner, alice, whitelistedUser1 },
             } = await loadFixture(initContracts);
-            await depositAndDelegateTo(st1inch, delegation, alice, owner.address, ether('8'));
+            await depositAndDelegateTo(st1inch, delegation, alice, owner, ether('8'));
             await whitelistRegistry.register();
 
             await delegation.connect(alice).delegate(constants.ZERO_ADDRESS);
@@ -136,10 +135,10 @@ describe('PowerPod', function () {
                 contracts: { st1inch, delegation, whitelistRegistry },
                 accounts: { owner, alice, whitelistedUser1 },
             } = await loadFixture(initContracts);
-            await depositAndDelegateTo(st1inch, delegation, alice, owner.address, ether('8'));
+            await depositAndDelegateTo(st1inch, delegation, alice, owner, ether('8'));
             await whitelistRegistry.register();
 
-            await delegation.connect(alice).delegate(whitelistedUser1.address);
+            await delegation.connect(alice).delegate(whitelistedUser1);
             await whitelistRegistry.connect(whitelistedUser1).register();
             expect(await whitelistRegistry.getWhitelist()).to.not.contain(owner.address);
         });
