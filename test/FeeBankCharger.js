0104f1880acc00ab3906c79c36b6b29723fd8479
Denis <zzomrot.ne@gmail.com>
2023-11-22 19:36:09 +0400

    Patch FeeBankCharger tests

--- test/FeeBankCharger.js
+++ test/FeeBankCharger.js
@@ -5,16 +5,16 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 describe('FeeBankCharger', function () {
     async function initContracts() {
         const [owner, alice] = await ethers.getSigners();
-        const inch = await deployContract('ERC20PermitMock', ['1INCH', '1INCH', owner.address, ether('1000')]);
+        const inch = await deployContract('ERC20PermitMock', ['1INCH', '1INCH', owner, ether('1000')]);
 
-        const charger = await deployContract('FeeBankCharger', [inch.address]);
+        const charger = await deployContract('FeeBankCharger', [inch]);
 
         const FeeBank = await ethers.getContractFactory('FeeBank');
         const feeBank = FeeBank.attach(await charger.feeBank());
 
-        await inch.transfer(alice.address, ether('100'));
-        await inch.approve(feeBank.address, ether('1000'));
-        await inch.connect(alice).approve(feeBank.address, ether('1000'));
+        await inch.transfer(alice, ether('100'));
+        await inch.approve(feeBank, ether('1000'));
+        await inch.connect(alice).approve(feeBank, ether('1000'));
 
         return {
             contracts: { inch, charger, feeBank },
@@ -26,14 +26,14 @@ describe('FeeBankCharger', function () {
         it('should increase credit', async function () {
             const { contracts: { charger, feeBank }, accounts: { alice } } = await loadFixture(initContracts);
             const amount = ether('100');
-            expect(await charger.availableCredit(alice.address)).to.equal('0');
-            await feeBank.depositFor(alice.address, amount);
-            expect(await charger.availableCredit(alice.address)).to.equal(amount);
+            expect(await charger.availableCredit(alice)).to.equal('0');
+            await feeBank.depositFor(alice, amount);
+            expect(await charger.availableCredit(alice)).to.equal(amount);
         });
 
         it('should not increase credit by non-feeBank address', async function () {
             const { contracts: { charger }, accounts: { alice } } = await loadFixture(initContracts);
-            await expect(charger.increaseAvailableCredit(alice.address, ether('100'))).to.be.revertedWithCustomError(
+            await expect(charger.increaseAvailableCredit(alice, ether('100'))).to.be.revertedWithCustomError(
                 charger,
                 'OnlyFeeBankAccess',
             );
@@ -52,14 +52,14 @@ describe('FeeBankCharger', function () {
         it('should decrease credit', async function () {
             const { contracts: { charger, feeBank }, accounts: { owner, alice }, others: { creditAmount } } = await loadFixture(initContractsAndAllowance);
             const amount = ether('10');
-            expect(await charger.availableCredit(owner.address)).to.equal(creditAmount);
-            await feeBank.withdrawTo(alice.address, amount);
-            expect(await charger.availableCredit(owner.address)).to.equal(creditAmount - amount);
+            expect(await charger.availableCredit(owner)).to.equal(creditAmount);
+            await feeBank.withdrawTo(alice, amount);
+            expect(await charger.availableCredit(owner)).to.equal(creditAmount - amount);
         });
 
         it('should not deccrease credit by non-feeBank address', async function () {
             const { contracts: { charger }, accounts: { alice } } = await loadFixture(initContractsAndAllowance);
-            await expect(charger.decreaseAvailableCredit(alice.address, ether('10'))).to.be.revertedWithCustomError(
+            await expect(charger.decreaseAvailableCredit(alice, ether('10'))).to.be.revertedWithCustomError(
                 charger,
                 'OnlyFeeBankAccess',
             );
