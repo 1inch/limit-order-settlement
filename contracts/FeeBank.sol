3e7d524fcf7c6235f3f3abdcc27eb61acb640d62
byshape <xenia.shape@gmail.com>
2024-06-28 16:02:18 +0100

    Allow floating pragma

--- contracts/FeeBank.sol
+++ contracts/FeeBank.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@1inch/solidity-utils/contracts/libraries/SafeERC20.sol";
