33bac23589cc36efd63315154c39f5a923f3bf70
Denis <zzomrot.ne@gmail.com>
2025-01-30 18:59:38 +0000

    Add deadline

--- contracts/KycNFT.sol
+++ contracts/KycNFT.sol
@@ -16,6 +16,8 @@ contract KycNFT is Ownable, ERC721Burnable {
     error OnlyOneNFTPerAddress();
     /// @notice Thrown when signature is incorrect.
     error BadSignature();
+    /// @notice Thrown when signature is expired.
+    error SignatureExpired();
 
     /// @notice Nonce for each token ID.
     mapping(uint256 => uint256) public nonces;
@@ -25,9 +27,10 @@ contract KycNFT is Ownable, ERC721Burnable {
      * @param tokenId The ID of the token.
      * @param signature The signature to be verified.
      */
-    modifier onlyOwnerSignature(address to, uint256 tokenId, bytes calldata signature) {
-        bytes memory message = abi.encodePacked(address(this), block.chainid, to, nonces[tokenId], tokenId);
-        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n136", message));
+    modifier onlyOwnerSignature(address to, uint256 tokenId, uint256 deadline, bytes calldata signature) {
+        if (block.timestamp > deadline) revert SignatureExpired();
+        bytes memory message = abi.encodePacked(address(this), block.chainid, to, nonces[tokenId], tokenId, deadline);
+        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n168", message));
         if (owner() != ECDSA.recover(hash, signature)) revert BadSignature();
         _;
     }
@@ -55,9 +58,10 @@ contract KycNFT is Ownable, ERC721Burnable {
      * @param from The address to transfer the token from.
      * @param to The address to transfer the token to.
      * @param tokenId The ID of the token to be transferred.
+     * @param deadline The deadline for the signature.
      * @param signature The signature of the owner permitting the transfer.
      */
-    function transferFrom(address from, address to, uint256 tokenId, bytes calldata signature) public onlyOwnerSignature(to, tokenId, signature) {
+    function transferFrom(address from, address to, uint256 tokenId, uint256 deadline, bytes calldata signature) public onlyOwnerSignature(to, tokenId, deadline, signature) {
         _transfer(from, to, tokenId);
     }
 
@@ -72,9 +76,10 @@ contract KycNFT is Ownable, ERC721Burnable {
 
     /**
      * @notice See {mint} method. This function using a valid owner's signature instead of only owner permission.
+     * @param deadline The deadline for the signature.
      * @param signature The signature of the owner permitting the mint.
      */
-    function mint(address to, uint256 tokenId, bytes calldata signature) external onlyOwnerSignature(to, tokenId, signature) {
+    function mint(address to, uint256 tokenId, uint256 deadline, bytes calldata signature) external onlyOwnerSignature(to, tokenId, deadline, signature) {
         _mint(to, tokenId);
     }
 
